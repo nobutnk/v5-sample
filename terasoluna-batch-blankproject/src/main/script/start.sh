dffa2fec0cac24f5219b36a5b70c9bcf5495ac91
nobutnk <nobutnk@gmail.com>
2018-02-21 00:13:38 +0900

    convert batch project to multi-project

--- terasoluna-batch-blankproject/src/main/script/start.sh
+++ terasoluna-batch-blankproject/src/main/script/start.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(cd $(dirname $0);pwd)
 PARENT_DIR=$(cd $(dirname $0);cd ..;pwd)
 
 
-AP_CLASSPATH="${PARENT_DIR}/*:${PARENT_DIR}/lib/*"
+AP_CLASSPATH="${PARENT_DIR}/conf:${PARENT_DIR}/lib/*"
 MAIN_CLASS="org.springframework.batch.core.launch.support.CommandLineJobRunner"
 
 java -cp "${AP_CLASSPATH}" ${MAIN_CLASS} META-INF/jobs/job01.xml job01
